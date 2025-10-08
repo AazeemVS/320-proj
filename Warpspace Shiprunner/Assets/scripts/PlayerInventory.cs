47f12a87cda07909da2e6f9f47ca2b97935434e0
C3llettuce <oor8644@rit.edu>
2025-10-08 09:30:41 -0400

    Added basic player inventory
    - Added inventory to player object
    - Has list of total inventory and active items
    - Can equip/unequip items

--- Warpspace Shiprunner/Assets/scripts/PlayerInventory.cs
+++ Warpspace Shiprunner/Assets/scripts/PlayerInventory.cs
@@ -1,16 +1,54 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerInventory : MonoBehaviour
 {
+    //currently assumes no slot specific/incompatible upgrades
+    public List<Upgrade> inventory;
+    public List<Upgrade> activeUpgrades;
+    private player_movement player;
+    private int maxInventory;
+    private int maxUpgrades;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GetComponentInParent<player_movement>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Add item/upgrade to player inventory
+    public bool GainItem(Upgrade newUpgrade) {
+        if(inventory.Count < maxInventory) {
+            inventory.Add(newUpgrade);
+            return true;
+        } 
+        return false;
+    }
+    //Remove item/upgrade from player inventory
+    public void RemoveItem(Upgrade newUpgrade) {
+        inventory.Remove(newUpgrade);
+    }
+    //Equip an upgrade from the player inventory
+    public bool EquipUpgrade(Upgrade newUpgrade) {
+        if (activeUpgrades.Count < maxUpgrades) {
+            activeUpgrades.Add(newUpgrade);
+            newUpgrade.OnEquip(player);
+            return true;
+        }
+        return false;
+    }
+    //Unequip a currently active upgrade
+    public void UnequipUpgrade(Upgrade newUpgrade) {
+        if (activeUpgrades.Contains(newUpgrade)) {
+            activeUpgrades.Remove(newUpgrade);
+            newUpgrade.OnUnequip(player);
+        }
     }
 }
--- Warpspace Shiprunner/Assets/scripts/Upgrade.cs
+++ Warpspace Shiprunner/Assets/scripts/Upgrade.cs
@@ -5,30 +5,29 @@ public abstract class Upgrade : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int rarity;
     public int value;
-    protected player_movement player;
     public 
     void Start()
     {
         
     }
-    public abstract void OnEquip();
-    public abstract void OnUnequip();
+    public abstract void OnEquip(player_movement player);
+    public abstract void OnUnequip(player_movement player);
 }
 
 public class EngineUpgrade : Upgrade {
     public float modifier;
-    public override void OnEquip() { player.moveSpeed += modifier; }
-    public override void OnUnequip() { player.moveSpeed -= modifier; }
+    public override void OnEquip(player_movement player) { player.moveSpeed += modifier; }
+    public override void OnUnequip(player_movement player) { player.moveSpeed -= modifier; }
 }
 
 public class DamageUpgrade : Upgrade {
     public float modifier;
-    public override void OnEquip() { player.playerDamage += modifier; }
-    public override void OnUnequip() { player.playerDamage -= modifier; }
+    public override void OnEquip(player_movement player) { player.playerDamage += modifier; }
+    public override void OnUnequip(player_movement player) { player.playerDamage -= modifier; }
 }
 
 public class AttackUpgrade : Upgrade {
     public float modifier;
-    public override void OnEquip() { player.shootTimerMax += modifier; }
-    public override void OnUnequip() { player.shootTimerMax -= modifier; }
+    public override void OnEquip(player_movement player) { player.shootTimerMax += modifier; }
+    public override void OnUnequip(player_movement player) { player.shootTimerMax -= modifier; }
 }
