fc571d83bb93bc77fda140251095956293e5e8f2
AazeemVS <av7304@rit.edu>
2025-11-05 15:37:21 -0500

    Refactored page switcher for new code architecture

--- Warpspace Shiprunner/Assets/scripts/Inventory Scripts/PageSwitcher.cs
+++ Warpspace Shiprunner/Assets/scripts/Inventory Scripts/PageSwitcher.cs
@@ -1,43 +1,25 @@
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PageSwitcher : MonoBehaviour
 {
-  // References to each background/page object
-  [SerializeField] private GameObject defaultBg;
   [SerializeField] private GameObject gearBg;
-  [SerializeField, CanBeNull] private GameObject tradeBg;
-  [SerializeField, CanBeNull] private GameObject shopBg;
 
-    // Called when the scene starts
-    void Start()
+  void Start()
   {
-    ShowDefault();     // Start with the default page visible
-    DisableRaycasts(); // Prevent background images from blocking clicks
+    // Always show the gear page
+    ShowGear();
+    DisableRaycasts();
   }
 
-  // Turns off raycast blocking for all background images
-  void DisableRaycasts()
+  private void DisableRaycasts()
   {
-    foreach (var go in new[] { defaultBg, gearBg, tradeBg, shopBg })
-      if (go && go.TryGetComponent<Image>(out var img))
-        img.raycastTarget = false;
+    if (gearBg && gearBg.TryGetComponent<Image>(out var img))
+      img.raycastTarget = false;
   }
 
-  // Functions to show each page
-  public void ShowDefault() => SetOnly(defaultBg);
-  public void ShowGear() => SetOnly(gearBg);
-  public void ShowTrade() => SetOnly(tradeBg);
-  public void ShowShop() => SetOnly(shopBg);
-
-  // Activates one page and hides all others
-  void SetOnly(GameObject target)
+  public void ShowGear()
   {
-    defaultBg?.SetActive(false);
-    gearBg?.SetActive(true);
-    tradeBg?.SetActive(false);
-    shopBg?.SetActive(false);
-    target?.SetActive(true);
+    if (gearBg) gearBg.SetActive(true);
   }
 }
